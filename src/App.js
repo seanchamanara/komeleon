87709d90316f7431ef6626a7bf0a551001a84d1d
seanchamanara <seanc@Seans-MacBook-Air.local>
2020-01-05 19:58:01 -0800

    made components for directory, menu/content, and homepage.  Added stylying to items

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import './App.css';
 
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage/homepage.component';
+
 
 function App() {
   return (
