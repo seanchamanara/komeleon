15d48ba15911c30df82ed81bdd8926f048ccbfd7
seanchamanara <seanc@Seans-MacBook-Air.local>
2020-02-19 15:35:53 -0800

    set up custom button.  Added firebase to project

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyB7RjnupTvqEvHsxf8cQHoDOMOjdOFy1HA",
+    authDomain: "komeleon-db.firebaseapp.com",
+    databaseURL: "https://komeleon-db.firebaseio.com",
+    projectId: "komeleon-db",
+    storageBucket: "komeleon-db.appspot.com",
+    messagingSenderId: "645681682472",
+    appId: "1:645681682472:web:4bc7568af58761a7b57d1d",
+    measurementId: "G-VHJ18VKT57"
+  };
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = ()  => auth.signinWithPopup(provider);
+
+export default firebase;
