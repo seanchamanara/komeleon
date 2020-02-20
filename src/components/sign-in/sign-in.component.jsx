fd973a6262f7a0e74f80b4f900ed4912b15cf33d
seanchamanara <seanc@Seans-MacBook-Air.local>
2020-02-19 18:57:38 -0800

    added Firebase Google authentication to sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,6 +5,7 @@ import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component{
     constructor(props) {
@@ -58,6 +59,7 @@ class SignIn extends React.Component{
                            required />
 
                     <CustomButton type='submit'>Sign in</CustomButton>
+                    <CustomButton onClick={signInWithGoogle}>Sign in with Google</CustomButton>
                 </form>
             
             </div>
