cc1c5f2570d56e8eb756403c3fc75c550a315b74
seanchamanara <seanc@Seans-MacBook-Air.local>
2020-03-19 14:59:42 -0700

    added cart icon and cart dropdown to application and cart reducers

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom';
 
 //"connect" is a higher order function, connecting to redux
 import { connect } from 'react-redux';
+
+
+
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -11,7 +16,7 @@ import './header.styles.scss';
 
 
 //currentUser de-structured from auth object
-const Header = ( { currentUser }) => (
+const Header = ( { currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -25,18 +30,25 @@ const Header = ( { currentUser }) => (
             </Link>
             
             {
-                currentUser ? 
+                currentUser ? (
                 <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
+                  )  :  (
                 <Link className='option' to='/signin'>SIGN IN</Link>
-            }
+                  )}
+            <CartIcon />
         </div>
+        {
+            hidden ? null : 
+            <CartDropdown />
+       
+        }
     </div>
 );
 
 
 //here state is reffering to the root reducer
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
-})
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
+});
 export default connect(mapStateToProps)(Header);
