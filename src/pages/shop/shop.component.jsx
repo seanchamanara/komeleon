609613bd9a23fca8dc8908a79c72ac0fde60d89b
seanchamanara <seanc@Seans-MacBook-Air.local>
2020-10-21 18:14:43 -0700

    updated shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -25,12 +25,20 @@ class ShopPage extends React.Component {
       const { updateCollections } = this.props;
       const collectionRef = firestore.collection('collections');
      
-      this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-       
-          const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-          updateCollections(collectionsMap);
-          this.setState({loading: false});
+      //Getting collections via fetch
+      // fetch('https://firestore.googleapis.com/v1/projects/komeleon-db/databases/(default)/documents/collections')
+      // .then(response => response.json())
+      // .then(collections => console.log(collections));
+
+
+
+      collectionRef.get().then(snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+       updateCollections(collectionsMap);
+       this.setState({loading: false});
       });
+
+
     }
 
     render(){
