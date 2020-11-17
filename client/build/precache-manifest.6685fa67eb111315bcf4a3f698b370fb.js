2aef8e65cc5422f0650151498b3fae98f750e7a7
seanchamanara <seanc@Seans-MacBook-Air.local>
2020-11-16 21:42:53 -0800

    updated .gitignore #production -- from /client/build to /build

---
+++ client/build/precache-manifest.6685fa67eb111315bcf4a3f698b370fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15dcef08aac48e42d3fd110c14d00b06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f30d31dca2fdc8275b83",
+    "url": "/static/css/main.c9d5907d.chunk.css"
+  },
+  {
+    "revision": "3c8c88f13a00c19b3022",
+    "url": "/static/js/2.ff264f94.chunk.js"
+  },
+  {
+    "revision": "6cac0f2d6fd8ecbe8ffbe3f8ff0d29f4",
+    "url": "/static/js/2.ff264f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f30d31dca2fdc8275b83",
+    "url": "/static/js/main.19be0e04.chunk.js"
+  },
+  {
+    "revision": "93b29f7e52cce678be69",
+    "url": "/static/js/runtime-main.48fc1031.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
